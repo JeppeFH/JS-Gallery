29512206f8746fc48100ebe9e3b23d5a44d7f243
JeppeFH <jeppefinch@hotmail.dk>
2024-06-18 11:37:50 +0200

    kommentar til JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,10 +11,10 @@ for (let index = 0; index < smallImgs.length; index++) {
     det store billede ændres til at vise det samme billede som det klikkede
      lille billede.*/
 
-    /*Fjerne active "class" */
+    /*Fjerne active "class" på alle img */
     document.querySelector("img.active").classList.remove("active");
 
-    /*Tilføje active "class" */
+    /*Tilføje active "class" på det click event der er aktiv */
     data.target.classList.add("active");
   });
 }
